4dcb6c52e38b97252772b442cdeb068822b64bbd
Ibigoni Inye-Tam <inyetamibigoni@gmail.com>
2025-06-12 04:25:08 -0500

    Remove default tests and add moeny tests

---
+++ test-jasmine/moneyTest.js
@@ -0,0 +1,20 @@
+import {formatCurrency} from '../javascript-amazon-project-main/scripts/utils/money.js';
+
+describe('test suite: formatCurrency', () => {
+  it('converts cents into dollars', () => {
+    expect(formatCurrency(2095)).toEqual('20.95');//Makes the test code easier to read
+  });
+  
+  it('works with 0', () =>{
+    expect(formatCurrency(0)).toEqual('0.00');
+  });
+
+  it('rounds up to the nearest cent', () => {
+    expect(formatCurrency(2000.5)).toEqual('20.01');
+  });
+
+  it('rounds down to the nearest cent', () => {
+    expect(formatCurrency(2000.4)).toEqual('20.00');
+  });
+});
+

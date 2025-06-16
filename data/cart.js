783f9a7cf1beef790e7a9ca6e06f6ffeddad3ae9
Ibigoni Inye-Tam <inyetamibigoni@gmail.com>
2025-06-16 18:49:19 -0500

    Create automated test for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,21 +1,27 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));//takes one string (name of what we saved earlier). Convert to regular string
+export let cart;
 
-//if there no item in localstorage it will result to null so there need to be a default value attached as shown below.
+loadFromStorage();
 
-if (!cart) {
-  cart = 
-  [{//getting the product Id gets all the other information about the product (i.e, name, image, price...)
-    productId:
-    'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionId: '1'
-  }, {
-    productId: 
-    '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '2'
-  }];
-  //This technique is called "Normalizing the data"
+export function loadFromStorage() {
+ cart = JSON.parse(localStorage.getItem('cart'));//takes one string (name of what we saved earlier). Convert to regular string
+
+  //if there no item in localstorage it will result to null so there need to be a default value attached as shown below.
+
+  if (!cart) {
+    cart = 
+    [{//getting the product Id gets all the other information about the product (i.e, name, image, price...)
+      productId:
+      'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    }, {
+      productId: 
+      '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
+    }];
+    //This technique is called "Normalizing the data"
+  }
 }
 
 //Using localStorage to save the cart from reseting when refreshing the webpage
--- test-jasmine/data/cartTest.js
+++ test-jasmine/data/cartTest.js
@@ -1,12 +1,43 @@
-import { addToCart, cart } from '../../data/cart.js';
+import { addToCart, cart, loadFromStorage } from '../../data/cart.js';
+document.body.innerHTML = `
+  <select class="js-cart-quantity-selector-test-product-id">
+    <option value="1" selected>1</option>
+  </select>
+  <div class="js-added-to-cart-test-product-id"></div>
+`;
 
 describe('test suite: addToCart', () => {
   it('adds an existing product to the cart', () => {
+    spyOn(localStorage, 'setItem');
 
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'test-product-id',
+        quantity: 1,
+        deliveryOptionId: '1'
+      }]);
+    });
+    loadFromStorage();
+
+    addToCart('test-product-id');
+    expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cart[0].productId).toEqual('test-product-id');
+    expect(cart[0].quantity).toEqual(2);
   });
 
   it('adds a new product to the cart', () => {
-    addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    spyOn(localStorage, 'setItem');
+
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([]);
+    });
+    loadFromStorage();
+
+    addToCart('test-product-id');
     expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cart[0].productId).toEqual('test-product-id');
+    expect(cart[0].quantity).toEqual(1);
   });
 });

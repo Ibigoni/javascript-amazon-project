f1a5446363e76ec33a219dbd32daaaa6ebf23231
Ibigoni Inye-Tam <inyetamibigoni@gmail.com>
2025-08-12 17:01:00 -0500

    Create order using the backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import {calculateCartQuantity, cart, updateQuantity} from '../../data/cart.js';
 import { getProduct } from '../../data/products.js';
 import { getDeliveryOption } from '../../data/deliveryOptions.js';
 import {formatCurrency} from '../utils/money.js';
+import {addOrder} from '../../data/orders.js';
 
 
 export function renderPaymentSummary() {
@@ -65,11 +66,41 @@ let shippingPriceCents = 0;
             </div>
       </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary js-place-order">
             Place your order
           </button>
  `;
 
  document.querySelector('.js-payment-summary')
  .innerHTML = paymentSummaryHTML;
+
+ document.querySelector('.js-place-order')
+  .addEventListener('click', async () => {//Making this an async function because we need to wait for the response to come back (backend).fetch returns a promise 
+    //sending data to the backend using POST. Need to give fetch and param which is an object
+    try {
+      const response = await fetch('https://supersimplebackend.dev/orders', { //waits for fetch to finish before moving on to next line.
+        method: 'POST', //type of request
+        headers: {//gives the backend more information about the request
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({//we can't send an object directly. We need to convert it into a JSON string by using JSON.stringify
+          cart: cart
+        })
+      });
+
+      const order = await response.json(); //response.json() is also a promist so we need to use await before going to the next line
+      addOrder(order);
+
+    } catch (error) {
+      console.log('Unexpected error. Try again later.')
+    }
+
+    //changes the url at the top of the browser  
+    window.location.href = 'orders.html';
+  });
+
+
+  /**NOTE: WE CAN ONLY USE AWAIT INSIDE AN ASYNC FUNCTION**/
+
+  //Here we send an http request to the backend to create the order
 }
